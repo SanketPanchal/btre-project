6214f93ce480d57511169158206ae8e8a0bb2209
Sanket Panchal <psan3954@uni.sydney.edu.au>
2018-12-06 20:02:00 +1100

    landingpage index

--- listings/views.py
+++ listings/views.py
@@ -3,15 +3,15 @@ from . models import Listing
 
 
 def index(request):
-
     listings = Listing.objects.all()
-    context = {
-        'listings':listings
-    }
-    return render(request,'listings/listings.html', context)
 
-def listing(request):
-    return render(request,'listings/listing.html')
+    context = {
+        'listings': listings
+        }
+    return render(request, 'listings/listings.html', context)
+
+def listing(request, listing_id):
+    return render(request, 'listings/listing.html') 
 
 def search(request):
-    return render(request,'listings/search.html')
+    return render(request, 'listings/search.html')
