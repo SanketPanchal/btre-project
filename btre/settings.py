43af480423cd6590cf1b0072082b1552555d0e92
Sanket Panchal <psan3954@uni.sydney.edu.au>
2018-12-07 10:27:27 +1100

    accounts app

--- btre/settings.py
+++ btre/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'pages.apps.PagesConfig',
     'realtors.apps.RealtorsConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
