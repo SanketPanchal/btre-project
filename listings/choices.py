d11c743dc7d11af770dc41a2949de330f507048c
Sanket Panchal <psan3954@uni.sydney.edu.au>
2018-12-06 23:42:13 +1100

    search query

---
+++ listings/choices.py
@@ -0,0 +1,39 @@
+bedroom_choices = {
+  '1':1,
+  '2':2,
+  '3':3,
+  '4':4,
+  '5':5,
+  '6':6,
+  '7':7,
+  '8':8,
+  '9':9,
+  '10':10
+  }
+
+price_choices = {
+  
+  '400000':'$400,000',
+  '600000':'$600,000',
+  '800000':'$800,000',
+  '1000000':'$1,000,000',
+  '1200000':'$1,200,000',
+  '1400000':'$1,400,000',
+  '1600000':'$1,600,000',
+  '1800000':'$1,800,000',
+  '2000000':'$2M+',
+
+
+
+}
+
+state_choices = {
+        'ACT': 'Australian Capital Territory',
+        'NSW': 'New South Wales',
+        'NT': 'Northern Territory',
+        'QLD': 'Queensland',
+        'SA': 'South Australia',
+        'TAS': 'Tasmania',
+        'VIC': 'Victoria',
+        'WA': 'Western Australia'
+}
