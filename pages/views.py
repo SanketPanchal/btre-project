c726c90b12138f7353c557371be524c2af88a2fd
Sanket Panchal <psan3954@uni.sydney.edu.au>
2018-12-06 21:47:56 +1100

    MVP of month

--- pages/views.py
+++ pages/views.py
@@ -14,7 +14,16 @@ def index(request):
     return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request,'pages/about.html')
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    # Get MVP
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    } 
+    return render(request,'pages/about.html', context)
 
 
 
